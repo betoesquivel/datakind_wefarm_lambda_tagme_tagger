d3e1b80732402c2eb8c6b0c671d648d9e21310a4
Beto Esquivel <ppbeto94@gmail.com>
2016-09-24 17:39:04 +0100

    Can read n messages and tag them

--- handler.py
+++ handler.py
@@ -2,6 +2,7 @@
 import csv
 import itertools
 import requests
+import json
 
 # unicode csv reading
 def unicode_csv_dict_reader(unicode_csv_data, dialect=csv.excel, **kwargs):
@@ -14,6 +15,7 @@ def utf_8_encoder(unicode_csv_data):
         yield line.encode('utf-8')
 def get_nth_from_iterable(n, iterable):
     return next(itertools.islice(iterable, n, n+1))
+
 
 # misc parsing of tagme response
 def merge_dicts(x, y):
@@ -45,8 +47,30 @@ tagme_payload = lambda text: {
 tagme_call = lambda text: requests.get(TAGME_URL.strip(), params=tagme_payload(text))
 tagme_tag = lambda text: merge_dicts(json_foolproof_loads(tagme_call(text.strip()).text), {u'raw': text})
 
-def hello(event, context):
-    csvfile = open('messages.csv')
+def tag_nth_message(n):
+    csvfile = open('messages.csv', 'r')
     reader = unicode_csv_dict_reader(csvfile)
-    msg = get_nth_from_iterable(0, reader)
-    return msg
+    msg = get_nth_from_iterable(n, reader)
+    return tagme_tag(msg['body'])
+
+def tag_nth_message_in_reader(n, reader):
+    msg = get_nth_from_iterable(n, reader)
+    return tagme_tag(msg['body'])
+
+def accum(vals, val):
+    if vals:
+        val -= (sum(map(lambda i: i + 1, vals)))
+    vals.append(val)
+    return vals
+
+def hello(event, context):
+    jobs = event.get('n', [1])
+    jobs.sort()
+    csvfile = open('messages.csv', 'r')
+    reader = unicode_csv_dict_reader(csvfile)
+
+    jobs_accum = reduce(accum, jobs, [])
+
+    tagged = map(lambda j: tag_nth_message_in_reader(j, reader), jobs_accum)
+
+    return map(lambda ta: ta['raw'], tagged)
